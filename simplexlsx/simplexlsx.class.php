d9cf5ccc6a4a4d1f5f1e87b5fa611961a44f056d
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2024-03-11 11:03:43 -0400

    Update simplexlsx.class.php
    replaced {} with []

--- simplexlsx/simplexlsx.class.php
+++ simplexlsx/simplexlsx.class.php
@@ -290,7 +290,7 @@ class SimpleXLSX {
 			$index = 0;
 
 			for ($i = $colLen-1; $i >= 0; $i--)
-				$index += (ord($col{$i}) - 64) * pow(26, $colLen-$i-1);
+				$index += (ord($col[$i]) - 64) * pow(26, $colLen-$i-1);
 
 			return array($index-1, $row-1);
 		} else
